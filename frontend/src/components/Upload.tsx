eceddb098d3ee6bfc663bfc713a69251c0c49766
Seah Chin Ying <scy.6char@gmail.com>
2020-03-30 19:24:44 +0800

    Obtain presigned url once file is selected

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react'
+import axios from 'axios'
 
 type Props = {}
 type State = {
-  selectedFile: File | null
+  s3Key: string | null,
+  selectedFile: File | null,
+  uploadId: string | null,
+}
+
+interface StartUploadResponse {
+  s3Key: string;
+  uploadId: string;
 }
 
 export default class Upload extends React.Component<Props, State> {
@@ -10,15 +18,37 @@ export default class Upload extends React.Component<Props, State> {
   constructor (props: Props) {
     super(props)
     this.state = {
-      selectedFile: null
+      s3Key: null,
+      selectedFile: null,
+      uploadId: null,
     }
   }
 
-  fileChangeHandler(event: React.SyntheticEvent<HTMLInputElement>) {
-    const file = event.currentTarget?.files?.[0]
-    console.log(file)
-    if (file === undefined) return
-    this.setState({ selectedFile: file })
+  private async obtainUploadId(): Promise<StartUploadResponse> {
+    try {
+      const resp = await axios.get('http://localhost:4000/upload/start')
+      return resp.data
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async fileChangeHandler(event: React.SyntheticEvent<HTMLInputElement>) {
+    try {
+      const file = event.currentTarget?.files?.[0]
+      console.log(file)
+      if (file === undefined) return
+      console.log(file.type)
+      this.setState({ selectedFile: file })
+
+      const startUploadResponse = await this.obtainUploadId()
+      this.setState({
+        s3Key: startUploadResponse.s3Key,
+        uploadId: startUploadResponse.uploadId
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   render () {
