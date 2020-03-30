86eb969012c9fba2ab5687f816c5152a31bae696
Seah Chin Ying <scy.6char@gmail.com>
2020-03-30 15:35:09 +0800

    Add aws-s3-sdk to backend

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,7 +1,17 @@
 import { Router, Request, Response } from 'express'
+import S3 = require('aws-sdk/clients/s3')
+
+const FILE_STORAGE_BUCKET_NAME = process.env.FILE_STORAGE_BUCKET_NAME as string
 
 const router = Router()
 
+const s3 = new S3({
+  signatureVersion: 'v4',
+  endpoint: FILE_STORAGE_BUCKET_NAME, // FIXME: change to envvar
+  s3BucketEndpoint: true
+})
+
+// here be handlers
 const presignedUrlHandler = async (_req: Request, res: Response) => {
   try {
     res.status(200).json({
