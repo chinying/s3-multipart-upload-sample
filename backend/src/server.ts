521efdacaffc45096acd6ba25351dd2331bea0b4
Seah Chin Ying <scy.6char@gmail.com>
2020-03-30 18:58:30 +0800

    Add cors to allow frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
+import cors from 'cors'
 import express from 'express'
 require('dotenv').config()
 
 import { UploadRoutes } from './routes/upload'
 
+const FRONTEND_URL = process.env.FRONTEND_URL as string
+
 const port = 4000
 const app: express.Application = express()
 const start = async (): Promise<void> => {
+  app.use(cors({
+    'origin': FRONTEND_URL
+  }))
   app.use('/upload/', UploadRoutes)
   app.listen(port, () => console.log(`Listening on port ${port}!`))
 }
