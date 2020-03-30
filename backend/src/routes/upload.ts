bde653bcf6dd99863655f0adfef8a581db5f716a
Seah Chin Ying <scy.6char@gmail.com>
2020-03-30 17:17:00 +0800

    Add multipart upload start

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,22 +1,34 @@
+import { promisify } from 'util'
 import { Router, Request, Response } from 'express'
+import { v4 as uuid } from 'uuid'
 import S3 = require('aws-sdk/clients/s3')
 
+const AWS_REGION = process.env.AWS_REGION as string
 const FILE_STORAGE_BUCKET_NAME = process.env.FILE_STORAGE_BUCKET_NAME as string
 
 const router = Router()
 
 const s3 = new S3({
   signatureVersion: 'v4',
-  endpoint: FILE_STORAGE_BUCKET_NAME, // FIXME: change to envvar
-  s3BucketEndpoint: true
+  region: AWS_REGION
 })
+
+const createMultipartUpload = promisify(s3.createMultipartUpload.bind(s3))
 
 // here be handlers
 const presignedUrlHandler = async (_req: Request, res: Response) => {
   try {
-    res.status(200).json({
-      url: 'your url here'
-    })
+    const s3Key = uuid()
+    const params = {
+      Bucket: FILE_STORAGE_BUCKET_NAME,
+      Key: s3Key,
+      ContentType: 'text/csv'
+    }
+
+    // @ts-ignore
+    const uploadedData = await createMultipartUpload(params)
+    res.status(200).json({ uploadedData })
+
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: 'Unable to generate presigned url' })
